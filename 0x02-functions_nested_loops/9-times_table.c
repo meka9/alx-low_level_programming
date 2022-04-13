83f8f9150d82f1c54676e113dd322f98c1b16ed0
meka9 <melkamuasaye21@gmail.com>
2022-04-13 05:11:27 -0700

    function that prints the 9 times table, starting with 0.

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,35 +1,41 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
- * times_table - prints the 9 times table, starting with 0
+ * times_table - Prints the 9 times_table.
+ *
+ * Return: Always 0.
  */
-void time_table(void)
+void times_table(void)
 {
-int i, j, k;
-for (i = 0; i < 10; i++)
-{
-for (j = 0; j < 10; j++)
-{
-k = j * i;
-if (j == 0)
-{
-_putchar(k + '0');
-}
-if (k < 10 && j != 0)
-{
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
-_putchar(k + '0');
-}
-else if (k >= 10)
-{
-_putchar(',');
-_putchar(' ');
-_putchar((k / 10) + '0');
-_putchar((k % 10) + '0');
-}
-}
-_putchar('\n');
-}
+	int i, j, result = 0, fc, sc;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			result = i * j;
+			if (j > 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				if (result >= 10 && result < 100)
+				{
+					fc = result / 10;
+					sc = result % 10;
+					_putchar('0' + fc);
+					_putchar('0' + sc);
+				}
+				else if (result < 10)
+				{
+					_putchar(' ');
+					_putchar('0' + result);
+				}
+			}
+			else
+			{
+				_putchar(result + '0');
+			}
+		}
+	_putchar('\n');
+	}
 }
