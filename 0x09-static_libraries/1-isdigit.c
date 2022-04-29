df02edcdfafe160a212cef55aaa9c79aaf58fcb7
meka9 <melkamuasaye21@gmail.com>
2022-04-29 04:45:24 -0700

    creat file

--- 0x09-static_libraries/1-isdigit.c
+++ 0x09-static_libraries/1-isdigit.c
@@ -1,16 +1,14 @@
 #include "main.h"
-
 /**
- * _isupper - checks is a character is uppercase
- * @c: carrier integer variable
- * Return: 1 if true, 0 if false
- */
+*_isdigit - function that verifies if a  character is a digit or not
+*@c: tested character
+*Return: returns 1 if it is uppercase , 0 if not
+*/
 
-int _isupper(int c)
+int _isdigit(int c)
 {
-	if (c >= 65 && c <= 90)
-	{
-		return (1);
-	}
-	return (0);
+if ((c >= 48) && (c <= 57))
+return (1);
+
+return (0);
 }
