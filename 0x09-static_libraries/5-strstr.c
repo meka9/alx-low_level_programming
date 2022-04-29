71f48436b4d9fac0c2e361522c91f9ecdb43bda6
meka9 <melkamuasaye21@gmail.com>
2022-04-29 05:12:50 -0700

    creat file

--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -1,32 +1,34 @@
 #include "main.h"
-
 /**
-* _strstr - function locate
-* @haystack: pointer to char
-* @needle: pointer to char
-* Return: 0
+*_strstr - The _strstr() function finds the first occurrence
+* of the substring needle in the string haystack.
+* The terminating null bytes (\0) are not compared
+*@haystack: string where the search is made
+*@needle: string whose occurence is searched in haystack
+*Return:Returns a pointer to the beginning of the located
+* substring, or NULL if the substring is not found.
 */
 
 char *_strstr(char *haystack, char *needle)
 {
-	char *result = haystack, *fneedle = needle;
+	int i;
+
+	if (*needle == 0)
+		return (haystack);
 
 	while (*haystack)
 	{
-		while (*needle)
+		i = 0;
+
+		if (haystack[i] == needle[i])
 		{
-			if (*haystack++ != *needle++)
-			{
-				break;
-			}
+			do {
+				if (needle[i + 1] == '\0')
+					return (haystack);
+				i++;
+			} while (haystack[i] == needle[i]);
 		}
-		if (!*needle)
-		{
-			return (result);
-		}
-		needle = fneedle;
-		result++;
-		haystack = result;
+		haystack++;
 	}
-	return (0);
+	return ('\0');
 }
