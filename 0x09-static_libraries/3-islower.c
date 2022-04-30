56f7bd0a9e91ff6b1a71b4719439ea997b760677
meka9 <melkamuasaye21@gmail.com>
2022-04-30 03:54:43 -0700

    creat file

--- 0x09-static_libraries/3-islower.c
+++ 0x09-static_libraries/3-islower.c
@@ -1,13 +1,19 @@
 #include "main.h"
 /**
-*_islower - controls if a character is in lowercase
-*@c: character to be verified
-*Return: return 0 or 1
-*/
+ *
+ * _islower - tests whether a character is a
+ *
+ * lowercase letter from the English alphabet.
+ *
+ * @c: character to test.
+ *
+ * Return: 1 if it is a lowercase letter, 0 if it is not a lowercase letter.
+ */
 int _islower(int c)
 {
-if (c >= 'a' && c <= 'z')
-return (1);
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
 
-return (0);
 }
