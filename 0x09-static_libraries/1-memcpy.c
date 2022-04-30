f0ee56d2d45271ad84ab6f322944184c4c800491
meka9 <melkamuasaye21@gmail.com>
2022-04-30 03:46:44 -0700

    creat file

--- 0x09-static_libraries/1-memcpy.c
+++ 0x09-static_libraries/1-memcpy.c
@@ -1,18 +1,16 @@
-#include "main.h"
 /**
-*_memcpy - The _memcpy() function copies n bytes
-* from memory area src to memory area dest
-*@dest:area where bytes are copied to
-*@src:area where bytes are copied from
-*@n:number of bytes to copy
-*Return: returns a pointer to n
-*/
+ * _memcpy - copies memory area
+ * @dest: pointer to destination memory area
+ * @src: pointer to source memory area
+ * @n: number of bytes to copy
+ * Return: pointer to destination memory area
+ */
+
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i = 0;
+	unsigned int i;
 
-	for (; i < n; i++)
+	for (i = 0; i < n; i++)
 		dest[i] = src[i];
-
 	return (dest);
 }
