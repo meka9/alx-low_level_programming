f5e86105c2d8dfaf9883b931265193393234597b
meka9 <melkamuasaye21@gmail.com>
2022-07-07 04:19:59 -0700

    a function that inserts a new node at a given position

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,48 +1,44 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - insert node at index
- * @h: Head pointer
- * @idx: Index
- * @n: Value
- *
- * Return: Node or NULL if fails
+ * insert_dnodeint_at_index - func to insert node at index
+ * @h: double ptr to the beginning of the list
+ * @idx: The index to insert new node at
+ * @n: The data to add to new node.
+ * Return: address of the new node or NULL on failure.
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *tmp;
-	dlistint_t *new;
-	unsigned int i = 0;
+	dlistint_t *new, *old;
+	unsigned int a;
 
-	tmp = *h;
-
-	if (h == NULL || (tmp == NULL && idx != 0))
+	if (h == NULL)
 		return (NULL);
 
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = n;
-	while (tmp != NULL)
+	old = *h;
+
+	for (a = 0; old != NULL && a < idx; a++)
+		old = old->next;
+
+	if (old == NULL && a == idx)
+		return (add_dnodeint_end(h, n));
+
+	else if (old != NULL)
 	{
-		if (idx == i)
-		{
-			new->prev = tmp->prev;
-			new->next = tmp;
-			(tmp->prev)->next = new;
-			tmp->prev = new;
-			return (new);
-		}
-		if ((idx - 1) == i && tmp->next == NULL)
-		{
-			return (add_dnodeint_end(h, n));
-		}
-		tmp = tmp->next;
-		i++;
+		new = malloc(sizeof(dlistint_t));
+
+		if (new == NULL)
+			return (NULL);
+		new->n = n;
+		old->prev->next = new;
+		new->prev = old->prev;
+		old->prev = new;
+		new->next = old;
+
+		return (new);
 	}
-	free(new);
 	return (NULL);
 }
