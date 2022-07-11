8813f2c95f6cba4c33a7d7ef1ad1a9d5b77eabc4
meka9 <melkamuasaye21@gmail.com>
2022-07-11 04:28:51 -0700

    edit

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://github.com/meka9/alx-low_level_programming/tree/master/0x18-dynamic_libraries/nrandom.so
+wget -P /tmp/ https://github.com/meka9/alx-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
 export LD_PRELOAD =/tmp/nrandom.so
